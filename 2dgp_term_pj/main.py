4868a7d33f04cbfa58eb6986143cfe6acf30a8b0
sanghyun han <gkstkdgus98@gmail.com>
2025-11-20 14:56:15 +0900

    동상 크기 조정 및 상호작용 거리 조정

--- 2dgp_term_pj/main.py
+++ 2dgp_term_pj/main.py
@@ -126,7 +126,7 @@ while running:
                     elif check_collision(player.x, player.y, EATING_PLANET_X, EATING_PLANET_Y, INTERACTION_DISTANCE):
                         current_state = STATE_VIEWING_ART
                         currently_viewing_art = ART_EATING_PLANET
-                    elif check_collision(player.x, player.y, VENUS_X, VENUS_Y, INTERACTION_DISTANCE):
+                    elif check_collision(player.x, player.y, VENUS_X, VENUS_Y, 100):
                         current_state = STATE_VIEWING_ART
                         currently_viewing_art = ART_VENUS
 
